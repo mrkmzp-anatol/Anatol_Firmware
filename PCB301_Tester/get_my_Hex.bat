6ea7187192abc2636fb8ca4efff2b26e02278904
mrkmzp_anatol <markomazepaa@gmail.com>
2024-09-05 09:52:59 +0300

    modify hex for test

---
+++ PCB301_Tester/get_my_Hex.bat
@@ -0,0 +1,2 @@
+del "D:\Anatol_Embed_GitHub\Anatol_Firmware\PCB301_Tester\PCB301_Tester.hex"
+copy "D:\Anatol_Embed_GitHub\PCB301_Tester\Debug\PCB301_Tester.hex" "D:\Anatol_Embed_GitHub\Anatol_Firmware\PCB301_Tester"
