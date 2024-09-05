dc9fadb2af42fb4ff7436a195b21b5c1002aa3ec
mrkmzp_anatol <markomazepaa@gmail.com>
2024-09-05 09:56:56 +0300

    remove useless script

--- PCB301_Tester/get_my_Hex.bat
+++
@@ -1,2 +0,0 @@
-del "D:\Anatol_Embed_GitHub\Anatol_Firmware\PCB301_Tester\PCB301_Tester.hex"
-copy "D:\Anatol_Embed_GitHub\PCB301_Tester\Debug\PCB301_Tester.hex" "D:\Anatol_Embed_GitHub\Anatol_Firmware\PCB301_Tester"
